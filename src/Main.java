af0e4d68580365aaba756f86bde2d2a5c3c2f96f
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-08 09:49:18 +0800

    正方形填充测试

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import org.lwjgl.LWJGLException;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 public class Main {
 	public void start() {
@@ -14,59 +13,35 @@ public class Main {
 			System.exit(0);
 		}
 
-		// init OpenGL here
+		// init OpenGL
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
 		while (!Display.isCloseRequested()) {
+			// Clear the screen and depth buffer
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-			// render OpenGL here
+			// set the color of the quad (R,G,B,A)
+			GL11.glColor3f(0.5f, 0.5f, 1.0f);
 
-			pollInput();
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(100, 100);
+			GL11.glVertex2f(100 + 200, 100);
+			GL11.glVertex2f(100 + 200, 100 + 200);
+			GL11.glVertex2f(100, 100 + 200);
+			GL11.glEnd();
+
 			Display.update();
 		}
 
 		Display.destroy();
 	}
 
-	public void pollInput() {
-
-		if (Mouse.isButtonDown(0)) {
-			int x = Mouse.getX();
-			int y = Mouse.getY();
-
-			System.out.println("MOUSE DOWN @ X: " + x + " Y: " + y);
-		}
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
-			System.out.println("SPACE KEY IS DOWN");
-		}
-
-		while (Keyboard.next()) {
-			if (Keyboard.getEventKeyState()) {
-				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
-					System.out.println("A Key Pressed");
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
-					System.out.println("S Key Pressed");
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
-					System.out.println("D Key Pressed");
-				}
-			} else {
-				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
-					System.out.println("A Key Released");
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
-					System.out.println("S Key Released");
-				}
-				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
-					System.out.println("D Key Released");
-				}
-			}
-		}
-	}
-
 	public static void main(String[] argv) {
-		Main inputExample = new Main();
-		inputExample.start();
+		Main quadExample = new Main();
+		quadExample.start();
 	}
 }
