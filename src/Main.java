314a73e31f994dff52f6831b7e8674ba67359781
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-08 14:12:40 +0800

    测试左击拿起，右击放下，Ctrl C复制

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,116 @@
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static org.lwjgl.opengl.GL11.*;
 
 public class Main {
-	public static void main(String[] args) {
+	private static final List<Box> shapes = new ArrayList<Box>(16);
+	private static boolean somethingIsSelected = false;
+	private static long lastColourChange;
+
+	public static void main(String args[]) {
 		try {
-			// Sets the width of the display to 640 and the height to 480
 			Display.setDisplayMode(new DisplayMode(640, 480));
-			// Sets the title of the display to "Episode 2 - Display"
-			Display.setTitle("Episode 3 - OpenGL Rendering");
-			// Creates and shows the display
+			Display.setTitle("Input Demo");
 			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 			Display.destroy();
 			System.exit(1);
 		}
-		// Enter the state that is required for modify the projection. Note that, in contrary to Java2D, the vertex
-		// coordinate system does not have to be equal to the window coordinate space. The invocation to glOrtho creates
-		// a 2D vertex coordinate system like this:
-		// Upper-Left:  (0,0)   Upper-Right:  (640,0)
-		// Bottom-Left: (0,480) Bottom-Right: (640,480)
-		// If you skip the glOrtho method invocation, the default 2D projection coordinate space will be like this:
-		// Upper-Left:  (-1,+1) Upper-Right:  (+1,+1)
-		// Bottom-Left: (-1,-1) Bottom-Right: (+1,-1)
+		shapes.add(new Box(15, 15));
+		shapes.add(new Box(100, 150));
 		glMatrixMode(GL_PROJECTION);
 		glOrtho(0, 640, 480, 0, 1, -1);
 		glMatrixMode(GL_MODELVIEW);
-		// While we aren't pressing the red button on the display
 		while (!Display.isCloseRequested()) {
-			// Clear the 2D contents of the window.
 			glClear(GL_COLOR_BUFFER_BIT);
-			// ">>" denotes a possibly modified piece of OpenGL documentation (http://www.opengl.org/sdk/docs/man/)
-			// >> glBegin and glEnd delimit the vertices that define a primitive or a group of like primitives.
-			// >> glBegin accepts a single argument that specifies how the vertices are interpreted.
-			// All upcoming vertex calls will be taken as points of a quadrilateral until glEnd is called. Since
-			// this primitive requires four vertices, we will have to call glVertex four times.
-			glBegin(GL_QUADS);
-			// >> glVertex commands are used within glBegin/glEnd pairs to specify point, line, and polygon vertices.
-			// >> glColor sets the current colour. (All subsequent calls to glVertex will be assigned this colour)
-			// >> The number after 'glVertex'/'glColor' indicates the amount of components. (xyzw/rgba)
-			// >> The character after the number indicates the type of arguments.
-			// >>      (for 'glVertex' = d: Double, f: Float, i: Integer)
-			// >>      (for 'glColor'  = d: Double, f: Float, b: Signed Byte, ub: Unsigned Byte)
-			glColor3f(1.0f, 0.0f, 0.0f);                    // Pure Green
-			glVertex2i(0, 0);                               // Upper-left
-			glColor3b((byte) 0, (byte) 127, (byte) 0);      // Pure Red
-			glVertex2d(640.0, 0.0);                         // Upper-right
-			glColor3ub((byte) 255, (byte) 255, (byte) 255); // White
-			glVertex2f(640.0f, 480.0f);                     // Bottom-right
-			glColor3d(0.0d, 0.0d, 1.0d);                    // Pure Blue
-			glVertex2i(0, 480);                             // Bottom-left
-			// If we put another four calls to glVertex2i here, a second quadrilateral will be drawn.
-			glEnd();
-			// Update the contents of the display and check for input.
+			while (Keyboard.next()) {
+				if (Keyboard.getEventKey() == Keyboard.KEY_C && Keyboard.getEventKeyState()) {
+					shapes.add(new Box(15, 15));
+				}
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+				Display.destroy();
+				System.exit(0);
+			}
+			for (final Box box : shapes) {
+				if (Mouse.isButtonDown(0) && box.isInBounds(Mouse.getX(), 480 - Mouse.getY()) && !somethingIsSelected) {
+					somethingIsSelected = true;
+					box.selected = true;
+				}
+				if (Mouse.isButtonDown(2) && box.isInBounds(Mouse.getX(), 480 - Mouse.getY()) && !somethingIsSelected) {
+					if ((System.currentTimeMillis() - lastColourChange) >= 200 /* milliseconds */) {
+						box.randomiseColors();
+						lastColourChange = System.currentTimeMillis();
+					}
+				}
+				if (Mouse.isButtonDown(1)) {
+					box.selected = false;
+					somethingIsSelected = false;
+				}
+
+				if (box.selected) {
+					box.update(Mouse.getDX(), -Mouse.getDY());
+				}
+
+				box.draw();
+			}
+
 			Display.update();
-			// Wait until we reach 60 frames-per-second.
 			Display.sync(60);
 		}
+
 		Display.destroy();
-		System.exit(0);
+	}
+
+	private static class Box {
+
+		public int x, y;
+		public boolean selected = false;
+		private float colorRed, colorBlue, colorGreen;
+
+		Box(int x, int y) {
+			this.x = x;
+			this.y = y;
+
+			Random randomGenerator = new Random();
+			colorRed = randomGenerator.nextFloat();
+			colorBlue = randomGenerator.nextFloat();
+			colorGreen = randomGenerator.nextFloat();
+		}
+
+		boolean isInBounds(int mouseX, int mouseY) {
+			return mouseX > x && mouseX < x + 50 && mouseY > y && mouseY < y + 50;
+		}
+
+		void update(int dx, int dy) {
+			x += dx;
+			y += dy;
+		}
+
+		void randomiseColors() {
+			Random randomGenerator = new Random();
+			colorRed = randomGenerator.nextFloat();
+			colorBlue = randomGenerator.nextFloat();
+			colorGreen = randomGenerator.nextFloat();
+		}
+
+		void draw() {
+			glColor3f(colorRed, colorGreen, colorBlue);
+			glBegin(GL_QUADS);
+			glVertex2f(x, y);
+			glVertex2f(x + 50, y);
+			glVertex2f(x + 50, y + 50);
+			glVertex2f(x, y + 50);
+			glEnd();
+		}
 	}
 }
