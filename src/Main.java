a62167144e0c4c7440a00497c7080e2a3d7ab10b
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-08 09:38:03 +0800

    测试键盘鼠标输入

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
@@ -18,14 +20,53 @@ public class Main {
 
 			// render OpenGL here
 
+			pollInput();
 			Display.update();
 		}
 
 		Display.destroy();
 	}
 
+	public void pollInput() {
+
+		if (Mouse.isButtonDown(0)) {
+			int x = Mouse.getX();
+			int y = Mouse.getY();
+
+			System.out.println("MOUSE DOWN @ X: " + x + " Y: " + y);
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			System.out.println("SPACE KEY IS DOWN");
+		}
+
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					System.out.println("A Key Pressed");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+					System.out.println("S Key Pressed");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+					System.out.println("D Key Pressed");
+				}
+			} else {
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					System.out.println("A Key Released");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+					System.out.println("S Key Released");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+					System.out.println("D Key Released");
+				}
+			}
+		}
+	}
+
 	public static void main(String[] argv) {
-		Main displayExample = new Main();
-		displayExample.start();
+		Main inputExample = new Main();
+		inputExample.start();
 	}
 }
